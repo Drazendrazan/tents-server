1360311502f8f27d2e89bd5d67f3c8dfbd0eb8e3
TukS <91913765+gustitriprayoga@users.noreply.github.com>
2022-05-31 22:24:33 +0700

    SQL backup 31 05 22

---
+++ backup31.sql
@@ -0,0 +1,505 @@
+-- --------------------------------------------------------
+-- Host:                         127.0.0.1
+-- Versi server:                 10.4.24-MariaDB - mariadb.org binary distribution
+-- OS Server:                    Win64
+-- HeidiSQL Versi:               11.3.0.6295
+-- --------------------------------------------------------
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+
+-- Membuang struktur basisdata untuk qbcoreframework_584b20
+CREATE DATABASE IF NOT EXISTS `qbcoreframework_584b20` /*!40100 DEFAULT CHARACTER SET utf8 */;
+USE `qbcoreframework_584b20`;
+
+-- membuang struktur untuk table qbcoreframework_584b20.apartments
+CREATE TABLE IF NOT EXISTS `apartments` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) DEFAULT NULL,
+  `type` varchar(255) DEFAULT NULL,
+  `label` varchar(255) DEFAULT NULL,
+  `citizenid` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `citizenid` (`citizenid`),
+  KEY `name` (`name`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+
+-- Pengeluaran data tidak dipilih.
+
+-- membuang struktur untuk table qbcoreframework_584b20.bank_accounts
+CREATE TABLE IF NOT EXISTS `bank_accounts` (
+  `record_id` bigint(255) NOT NULL AUTO_INCREMENT,
+  `citizenid` varchar(250) DEFAULT NULL,
+  `business` varchar(50) DEFAULT NULL,
+  `businessid` int(11) DEFAULT NULL,
+  `gangid` varchar(50) DEFAULT NULL,
+  `amount` bigint(255) NOT NULL DEFAULT 0,
+  `account_type` enum('Current','Savings','Business','Gang') NOT NULL DEFAULT 'Current',
+  PRIMARY KEY (`record_id`),
+  UNIQUE KEY `citizenid` (`citizenid`),
+  KEY `business` (`business`),
+  KEY `businessid` (`businessid`),
+  KEY `gangid` (`gangid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- Pengeluaran data tidak dipilih.
+
+-- membuang struktur untuk table qbcoreframework_584b20.bank_statements
+CREATE TABLE IF NOT EXISTS `bank_statements` (
+  `record_id` bigint(255) NOT NULL AUTO_INCREMENT,
+  `citizenid` varchar(50) DEFAULT NULL,
+  `account` varchar(50) DEFAULT NULL,
+  `business` varchar(50) DEFAULT NULL,
+  `businessid` int(11) DEFAULT NULL,
+  `gangid` varchar(50) DEFAULT NULL,
+  `deposited` int(11) DEFAULT NULL,
+  `withdraw` int(11) DEFAULT NULL,
+  `balance` int(11) DEFAULT NULL,
+  `date` varchar(50) DEFAULT NULL,
+  `type` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`record_id`),
+  KEY `business` (`business`),
+  KEY `businessid` (`businessid`),
+  KEY `gangid` (`gangid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- Pengeluaran data tidak dipilih.
+
+-- membuang struktur untuk table qbcoreframework_584b20.bans
+CREATE TABLE IF NOT EXISTS `bans` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) DEFAULT NULL,
+  `license` varchar(50) DEFAULT NULL,
+  `discord` varchar(50) DEFAULT NULL,
+  `ip` varchar(50) DEFAULT NULL,
+  `reason` text DEFAULT NULL,
+  `expire` int(11) DEFAULT NULL,
+  `bannedby` varchar(255) NOT NULL DEFAULT 'LeBanhammer',
+  PRIMARY KEY (`id`),
+  KEY `license` (`license`),
+  KEY `discord` (`discord`),
+  KEY `ip` (`ip`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- Pengeluaran data tidak dipilih.
+
+-- membuang struktur untuk table qbcoreframework_584b20.battlepass
+CREATE TABLE IF NOT EXISTS `battlepass` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `player` varchar(255) COLLATE utf8mb4_turkish_ci NOT NULL,
+  `data` text COLLATE utf8mb4_turkish_ci NOT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE=InnoDB AUTO_INCREMENT=18 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_turkish_ci ROW_FORMAT=DYNAMIC;
+
+-- Pengeluaran data tidak dipilih.
+
+-- membuang struktur untuk table qbcoreframework_584b20.crypto
+CREATE TABLE IF NOT EXISTS `crypto` (
+  `crypto` varchar(50) NOT NULL DEFAULT 'qbit',
+  `worth` int(11) NOT NULL DEFAULT 0,
+  `history` text DEFAULT NULL,
+  PRIMARY KEY (`crypto`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- Pengeluaran data tidak dipilih.
+
+-- membuang struktur untuk table qbcoreframework_584b20.crypto_transactions
+CREATE TABLE IF NOT EXISTS `crypto_transactions` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `citizenid` varchar(50) DEFAULT NULL,
+  `title` varchar(50) DEFAULT NULL,
+  `message` varchar(50) DEFAULT NULL,
+  `date` timestamp NULL DEFAULT current_timestamp(),
+  PRIMARY KEY (`id`),
+  KEY `citizenid` (`citizenid`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+
+-- Pengeluaran data tidak dipilih.
+
+-- membuang struktur untuk table qbcoreframework_584b20.dealers
+CREATE TABLE IF NOT EXISTS `dealers` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) NOT NULL DEFAULT '0',
+  `coords` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin DEFAULT NULL,
+  `time` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin DEFAULT NULL,
+  `createdby` varchar(50) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- Pengeluaran data tidak dipilih.
+
+-- membuang struktur untuk table qbcoreframework_584b20.gloveboxitems
+CREATE TABLE IF NOT EXISTS `gloveboxitems` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `plate` varchar(255) NOT NULL DEFAULT '[]',
+  `items` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin DEFAULT NULL,
+  PRIMARY KEY (`plate`),
+  KEY `id` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+
+-- Pengeluaran data tidak dipilih.
+
+-- membuang struktur untuk table qbcoreframework_584b20.houselocations
+CREATE TABLE IF NOT EXISTS `houselocations` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) DEFAULT NULL,
+  `label` varchar(255) DEFAULT NULL,
+  `coords` text DEFAULT NULL,
+  `owned` tinyint(1) DEFAULT NULL,
+  `price` int(11) DEFAULT NULL,
+  `tier` tinyint(4) DEFAULT NULL,
+  `garage` text DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `name` (`name`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+
+-- Pengeluaran data tidak dipilih.
+
+-- membuang struktur untuk table qbcoreframework_584b20.house_plants
+CREATE TABLE IF NOT EXISTS `house_plants` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `building` varchar(50) DEFAULT NULL,
+  `stage` varchar(50) DEFAULT 'stage-a',
+  `sort` varchar(50) DEFAULT NULL,
+  `gender` varchar(50) DEFAULT NULL,
+  `food` int(11) DEFAULT 100,
+  `health` int(11) DEFAULT 100,
+  `progress` int(11) DEFAULT 0,
+  `coords` text DEFAULT NULL,
+  `plantid` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `building` (`building`),
+  KEY `plantid` (`plantid`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+
+-- Pengeluaran data tidak dipilih.
+
+-- membuang struktur untuk table qbcoreframework_584b20.lapraces
+CREATE TABLE IF NOT EXISTS `lapraces` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) DEFAULT NULL,
+  `checkpoints` text DEFAULT NULL,
+  `records` text DEFAULT NULL,
+  `creator` varchar(50) DEFAULT NULL,
+  `distance` int(11) DEFAULT NULL,
+  `raceid` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `raceid` (`raceid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- Pengeluaran data tidak dipilih.
+
+-- membuang struktur untuk table qbcoreframework_584b20.management_funds
+CREATE TABLE IF NOT EXISTS `management_funds` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `job_name` varchar(50) NOT NULL,
+  `amount` int(100) NOT NULL,
+  `type` enum('boss','gang') NOT NULL DEFAULT 'boss',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `job_name` (`job_name`),
+  KEY `type` (`type`)
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8;
+
+-- Pengeluaran data tidak dipilih.
+
+-- membuang struktur untuk table qbcoreframework_584b20.occasion_vehicles
+CREATE TABLE IF NOT EXISTS `occasion_vehicles` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `seller` varchar(50) DEFAULT NULL,
+  `price` int(11) DEFAULT NULL,
+  `description` longtext DEFAULT NULL,
+  `plate` varchar(50) DEFAULT NULL,
+  `model` varchar(50) DEFAULT NULL,
+  `mods` text DEFAULT NULL,
+  `occasionid` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `occasionId` (`occasionid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- Pengeluaran data tidak dipilih.
+
+-- membuang struktur untuk table qbcoreframework_584b20.phone_gallery
+CREATE TABLE IF NOT EXISTS `phone_gallery` (
+  `citizenid` varchar(255) NOT NULL,
+  `image` varchar(255) NOT NULL,
+  `date` timestamp NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- Pengeluaran data tidak dipilih.
+
+-- membuang struktur untuk table qbcoreframework_584b20.phone_invoices
+CREATE TABLE IF NOT EXISTS `phone_invoices` (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `citizenid` varchar(50) DEFAULT NULL,
+  `amount` int(11) NOT NULL DEFAULT 0,
+  `society` tinytext DEFAULT NULL,
+  `sender` varchar(50) DEFAULT NULL,
+  `sendercitizenid` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `citizenid` (`citizenid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- Pengeluaran data tidak dipilih.
+
+-- membuang struktur untuk table qbcoreframework_584b20.phone_messages
+CREATE TABLE IF NOT EXISTS `phone_messages` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `citizenid` varchar(50) DEFAULT NULL,
+  `number` varchar(50) DEFAULT NULL,
+  `messages` text DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `citizenid` (`citizenid`),
+  KEY `number` (`number`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- Pengeluaran data tidak dipilih.
+
+-- membuang struktur untuk table qbcoreframework_584b20.phone_tweets
+CREATE TABLE IF NOT EXISTS `phone_tweets` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `citizenid` varchar(50) DEFAULT NULL,
+  `firstName` varchar(25) DEFAULT NULL,
+  `lastName` varchar(25) DEFAULT NULL,
+  `message` text DEFAULT NULL,
+  `date` datetime DEFAULT current_timestamp(),
+  `url` text DEFAULT NULL,
+  `picture` varchar(512) DEFAULT './img/default.png',
+  `tweetId` varchar(25) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `citizenid` (`citizenid`)
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8;
+
+-- Pengeluaran data tidak dipilih.
+
+-- membuang struktur untuk table qbcoreframework_584b20.players
+CREATE TABLE IF NOT EXISTS `players` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `citizenid` varchar(50) NOT NULL,
+  `cid` int(11) DEFAULT NULL,
+  `license` varchar(255) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `money` text NOT NULL,
+  `charinfo` text DEFAULT NULL,
+  `job` text NOT NULL,
+  `gang` text DEFAULT NULL,
+  `position` text NOT NULL,
+  `metadata` text NOT NULL,
+  `inventory` longtext DEFAULT NULL,
+  `last_updated` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `skin` longtext DEFAULT NULL,
+  `tattoos` longtext DEFAULT NULL,
+  PRIMARY KEY (`citizenid`),
+  KEY `id` (`id`),
+  KEY `last_updated` (`last_updated`),
+  KEY `license` (`license`)
+) ENGINE=InnoDB AUTO_INCREMENT=747 DEFAULT CHARSET=utf8;
+
+-- Pengeluaran data tidak dipilih.
+
+-- membuang struktur untuk table qbcoreframework_584b20.playerskins
+CREATE TABLE IF NOT EXISTS `playerskins` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `citizenid` varchar(255) NOT NULL,
+  `model` varchar(255) NOT NULL,
+  `skin` text NOT NULL,
+  `active` tinyint(4) NOT NULL DEFAULT 1,
+  PRIMARY KEY (`id`),
+  KEY `citizenid` (`citizenid`),
+  KEY `active` (`active`)
+) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=utf8;
+
+-- Pengeluaran data tidak dipilih.
+
+-- membuang struktur untuk table qbcoreframework_584b20.players_appearance
+CREATE TABLE IF NOT EXISTS `players_appearance` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `citizenid` varchar(50) DEFAULT NULL,
+  `model` varchar(50) DEFAULT NULL,
+  `appearance` longtext DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id_UNIQUE` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+
+-- Pengeluaran data tidak dipilih.
+
+-- membuang struktur untuk table qbcoreframework_584b20.players_outfits
+CREATE TABLE IF NOT EXISTS `players_outfits` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `citizenid` varchar(50) DEFAULT NULL,
+  `name` longtext DEFAULT NULL,
+  `ped` longtext DEFAULT NULL,
+  `components` longtext DEFAULT NULL,
+  `props` longtext DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id_UNIQUE` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- Pengeluaran data tidak dipilih.
+
+-- membuang struktur untuk table qbcoreframework_584b20.players_pd_presets
+CREATE TABLE IF NOT EXISTS `players_pd_presets` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` longtext DEFAULT NULL,
+  `ped` longtext DEFAULT NULL,
+  `components` longtext DEFAULT NULL,
+  `props` longtext DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id_UNIQUE` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- Pengeluaran data tidak dipilih.
+
+-- membuang struktur untuk table qbcoreframework_584b20.player_contacts
+CREATE TABLE IF NOT EXISTS `player_contacts` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `citizenid` varchar(50) DEFAULT NULL,
+  `name` varchar(50) DEFAULT NULL,
+  `number` varchar(50) DEFAULT NULL,
+  `iban` varchar(50) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`),
+  KEY `citizenid` (`citizenid`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+
+-- Pengeluaran data tidak dipilih.
+
+-- membuang struktur untuk table qbcoreframework_584b20.player_houses
+CREATE TABLE IF NOT EXISTS `player_houses` (
+  `id` int(255) NOT NULL AUTO_INCREMENT,
+  `house` varchar(50) NOT NULL,
+  `identifier` varchar(50) DEFAULT NULL,
+  `citizenid` varchar(50) DEFAULT NULL,
+  `keyholders` text DEFAULT NULL,
+  `decorations` text DEFAULT NULL,
+  `stash` text DEFAULT NULL,
+  `outfit` text DEFAULT NULL,
+  `logout` text DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `house` (`house`),
+  KEY `citizenid` (`citizenid`),
+  KEY `identifier` (`identifier`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+
+-- Pengeluaran data tidak dipilih.
+
+-- membuang struktur untuk table qbcoreframework_584b20.player_mails
+CREATE TABLE IF NOT EXISTS `player_mails` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `citizenid` varchar(50) DEFAULT NULL,
+  `sender` varchar(50) DEFAULT NULL,
+  `subject` varchar(50) DEFAULT NULL,
+  `message` text DEFAULT NULL,
+  `read` tinyint(4) DEFAULT 0,
+  `mailid` int(11) DEFAULT NULL,
+  `date` timestamp NULL DEFAULT current_timestamp(),
+  `button` text DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `citizenid` (`citizenid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- Pengeluaran data tidak dipilih.
+
+-- membuang struktur untuk table qbcoreframework_584b20.player_outfits
+CREATE TABLE IF NOT EXISTS `player_outfits` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `citizenid` varchar(50) DEFAULT NULL,
+  `name` longtext DEFAULT NULL,
+  `ped` longtext DEFAULT NULL,
+  `components` longtext DEFAULT NULL,
+  `props` longtext DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id_UNIQUE` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8;
+
+-- Pengeluaran data tidak dipilih.
+
+-- membuang struktur untuk table qbcoreframework_584b20.player_vehicles
+CREATE TABLE IF NOT EXISTS `player_vehicles` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `license` varchar(50) DEFAULT NULL,
+  `citizenid` varchar(50) DEFAULT NULL,
+  `vehicle` varchar(50) DEFAULT NULL,
+  `hash` varchar(50) DEFAULT NULL,
+  `mods` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin DEFAULT NULL,
+  `plate` varchar(50) NOT NULL,
+  `fakeplate` varchar(50) DEFAULT NULL,
+  `garage` varchar(50) DEFAULT NULL,
+  `fuel` int(11) DEFAULT 100,
+  `engine` float DEFAULT 1000,
+  `body` float DEFAULT 1000,
+  `state` int(11) DEFAULT 1,
+  `depotprice` int(11) NOT NULL DEFAULT 0,
+  `drivingdistance` int(50) DEFAULT NULL,
+  `status` text DEFAULT NULL,
+  `balance` int(11) NOT NULL DEFAULT 0,
+  `paymentamount` int(11) NOT NULL DEFAULT 0,
+  `paymentsleft` int(11) NOT NULL DEFAULT 0,
+  `financetime` int(11) NOT NULL DEFAULT 0,
+  PRIMARY KEY (`id`),
+  KEY `plate` (`plate`),
+  KEY `citizenid` (`citizenid`),
+  KEY `license` (`license`)
+) ENGINE=InnoDB AUTO_INCREMENT=33 DEFAULT CHARSET=utf8;
+
+-- Pengeluaran data tidak dipilih.
+
+-- membuang struktur untuk table qbcoreframework_584b20.player_warns
+CREATE TABLE IF NOT EXISTS `player_warns` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `senderIdentifier` varchar(50) DEFAULT NULL,
+  `targetIdentifier` varchar(50) DEFAULT NULL,
+  `reason` text DEFAULT NULL,
+  `warnId` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- Pengeluaran data tidak dipilih.
+
+-- membuang struktur untuk table qbcoreframework_584b20.scenes
+CREATE TABLE IF NOT EXISTS `scenes` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `creator` varchar(50) COLLATE utf8mb4_bin DEFAULT NULL,
+  `text` mediumtext COLLATE utf8mb4_bin DEFAULT NULL,
+  `color` mediumtext COLLATE utf8mb4_bin DEFAULT NULL,
+  `viewdistance` int(11) DEFAULT NULL,
+  `expiration` int(11) DEFAULT NULL,
+  `fontsize` decimal(10,1) DEFAULT NULL,
+  `fontstyle` int(11) DEFAULT NULL,
+  `coords` mediumtext COLLATE utf8mb4_bin DEFAULT NULL,
+  `date_creation` datetime DEFAULT NULL,
+  `date_deletion` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin ROW_FORMAT=DYNAMIC;
+
+-- Pengeluaran data tidak dipilih.
+
+-- membuang struktur untuk table qbcoreframework_584b20.stashitems
+CREATE TABLE IF NOT EXISTS `stashitems` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `stash` varchar(255) NOT NULL DEFAULT '[]',
+  `items` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin DEFAULT NULL,
+  PRIMARY KEY (`stash`),
+  KEY `id` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8;
+
+-- Pengeluaran data tidak dipilih.
+
+-- membuang struktur untuk table qbcoreframework_584b20.trunkitems
+CREATE TABLE IF NOT EXISTS `trunkitems` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `plate` varchar(255) NOT NULL DEFAULT '[]',
+  `items` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin DEFAULT NULL,
+  PRIMARY KEY (`plate`),
+  KEY `id` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- Pengeluaran data tidak dipilih.
+
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40111 SET SQL_NOTES=IFNULL(@OLD_SQL_NOTES, 1) */;
